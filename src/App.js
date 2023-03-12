4d233025744d2c0d7e247a0a607568a38dc81016
Kiara Melocoton <kiaramelocoton@gmail.com>
2023-03-12 01:16:12 -0800

    button for routes alternatives + map updates on click

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {
   Input,
   SkeletonText,
   Text,
+  VStack,
 } from '@chakra-ui/react'
 import { FaLocationArrow, FaTimes } from 'react-icons/fa'
 
@@ -30,6 +31,8 @@ function App() {
 
   const [map, setMap] = useState(/** @type google.maps.Map */ (null))
   const [directionsResponse, setDirectionsResponse] = useState(null)
+  const [routes, setRoutes] = useState([]);
+  const [routeIndex, setRouteIndex] = useState(0);
   const [distance, setDistance] = useState('')
   const [duration, setDuration] = useState('')
 
@@ -56,17 +59,24 @@ function App() {
       provideRouteAlternatives: true,
     })
     setDirectionsResponse(results)
+    setRoutes(results.routes);
     setDistance(results.routes[0].legs[0].distance.text)
     setDuration(results.routes[0].legs[0].duration.text)
   }
 
   function clearRoute() {
     setDirectionsResponse(null)
+    setRoutes([]);
+    setRouteIndex(0);
     setDistance('')
     setDuration('')
     originRef.current.value = ''
     destinationRef.current.value = ''
   }
+
+  const handleButtonClick = (index) => {
+    setRouteIndex(index);
+  };
 
   return (
     <Flex
@@ -90,9 +100,11 @@ function App() {
           }}
           onLoad={map => setMap(map)}
         >
-          <Marker position={center} />
+          <Marker position={center} visible={false} />
           {directionsResponse && (
-            <DirectionsRenderer directions={directionsResponse} />
+            <DirectionsRenderer 
+              directions={directionsResponse} 
+              routeIndex={routeIndex} />
           )}
         </GoogleMap>
       </Box>
@@ -145,6 +157,33 @@ function App() {
             }}
           />
         </HStack>
+        
+      {/* Available Routes */}
+      <VStack spacing={4} mt={4} justifyContent='space-between'>
+        {directionsResponse && (
+          <>
+            <Text fontSize="lg" fontWeight="bold" mt={4}>Available routes:</Text>
+            {routes.map((route, index) => (
+              <Button key={index} 
+                width="auto" 
+                height="auto" 
+                justifyContent="flex-start" 
+                textAlign="left" 
+                bgColor="transparent" 
+                _hover={{ bgColor: 'gray.100' }}
+                onClick={() => handleButtonClick(index)}>
+                <VStack>
+                  <div>Route {index + 1}: {route.summary}</div>
+                  <div>Distance: {route.legs[0].distance.text}</div>
+                  <div>Duration: {route.legs[0].duration.text}</div>
+                </VStack>
+              </Button>
+            ))}
+
+            <DirectionsRenderer directions={directionsResponse} routeIndex={routeIndex} />
+          </>
+        )}
+      </VStack>
       </Box>
     </Flex>
   )
