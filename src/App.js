0f267130730f0ea175f4bddf3b0d0e702cb23648
kahofrkw <kahofrkw@gmail.com>
2023-03-12 11:53:49 -0700

    Added Routing Functionality Buttons (Hide and Show)

--- src/App.js
+++ src/App.js
@@ -10,14 +10,18 @@ import {
   SkeletonText,
   Text,
   VStack,
-  Avatar, Alert, AlertIcon, Spacer,
+  Avatar,
+  Alert,
+  AlertIcon,
+  Spacer,
 } from "@chakra-ui/react";
 import { FaLocationArrow, FaTimes } from "react-icons/fa";
 import { FaMapMarkerAlt } from "react-icons/fa";
-import { BiTargetLock } from "react-icons/bi";
-import { AiOutlineHome } from "react-icons/ai";
+import { BiTargetLock, BiPhoneCall } from "react-icons/bi";
+import { AiOutlineHome, AiOutlineLeft } from "react-icons/ai";
 import { RxCounterClockwiseClock } from "react-icons/rx";
 import { MdFavoriteBorder } from "react-icons/md";
+import { HiBellAlert } from "react-icons/hi2";
 import {
   useJsApiLoader,
   GoogleMap,
@@ -42,6 +46,8 @@ function App() {
   const [directionsResponse, setDirectionsResponse] = useState(null);
   const [routes, setRoutes] = useState([]);
   const [routeIndex, setRouteIndex] = useState(0);
+  const [isVisible, setIsVisible] = useState(true);
+  const [isReturnVisible, setReturnVisible] = useState(false);
   const [distance, setDistance] = useState("");
   const [duration, setDuration] = useState("");
   const [now, setNow] = useState("");
@@ -61,11 +67,11 @@ function App() {
       "NW Marine Dr": 0.3650033853333333,
     };
     if (querySet[summary] >= 0.75) {
-      return "Danger"
+      return "Danger";
     } else if (querySet[summary] >= 0.4) {
-      return "Caution"
+      return "Caution";
     } else {
-      return "Safe Route"
+      return "Safe Route";
     }
   }
 
@@ -90,10 +96,14 @@ function App() {
     setDistance(results.routes[0].legs[0].distance.text);
     setDuration(results.routes[0].legs[0].duration.text);
     getNow();
-    setDangerLevel(routes.map((route) => {
-      getDangerLevel(route.summary);
-      console.log(`route summary: ${route.summary}, danger level: ${dangerLevel}`);
-    }));
+    setDangerLevel(
+      routes.map((route) => {
+        getDangerLevel(route.summary);
+        console.log(
+          `route summary: ${route.summary}, danger level: ${dangerLevel}`
+        );
+      })
+    );
   }
 
   function clearRoute() {
@@ -113,19 +123,27 @@ function App() {
   function getNow() {
     const today = new Date();
     let time = today.getHours() + ":" + today.getMinutes();
-    if (today.getMinutes()<10) {
-       time = today.getHours() + ":0" + today.getMinutes();
+    if (today.getMinutes() < 10) {
+      time = today.getHours() + ":0" + today.getMinutes();
     }
     setNow(time);
   }
   function calculateEnd(duration) {
     const today = new Date();
-    let newDateObj = new Date(today.getTime() + duration*1000);
-    if (newDateObj.getMinutes()<10) {
+    let newDateObj = new Date(today.getTime() + duration * 1000);
+    if (newDateObj.getMinutes() < 10) {
       return newDateObj.getHours() + ":0" + newDateObj.getMinutes();
     }
     return newDateObj.getHours() + ":" + newDateObj.getMinutes();
   }
+  const hideBoxes = () => {
+    setIsVisible(false);
+  };
+
+  const showReturnButton = () => {
+    isReturnVisible(true);
+    setReturnVisible(false);
+  };
 
   return (
     <Flex
@@ -158,225 +176,315 @@ function App() {
           )}
         </GoogleMap>
       </Box>
-
       {
         // Greeting Stack
       }
-      <HStack
-        marginTop={2}
-        p={2}
-        borderRadius="lg"
-        zIndex="1"
-        spacing={[
-          "150", // 0-30em
-          "300", // 30em-48em
-          "580", // 48em-62em
-          "800", // 62em+
-        ]}
-      >
-        <Text fontSize="2xl" as="b">
-          {" "}
-          Hi Kate!{" "}
-        </Text>
-        <Avatar
-          name="Kate Simnac"
-          alignItems
-          src="https://bit.ly/dan-abramov"
-        />
-      </HStack>
-
-      {
-        // Current Destination Search Bar
-      }
-      <Box
-        p={2}
-        borderRadius="lg"
-        m={1}
-        bgColor="white"
-        shadow="base"
-        zIndex="1"
-        width={[
-          "80%", // 0-30em
-          "50%", // 30em-48em
-          "25%", // 48em-62em
-          "15%", // 62em+
-        ]}
-      >
-        <HStack p={0.5} borderRadius="lg" zIndex="1">
-          <Icon as={BiTargetLock} boxSize={6} />
-          <Box flexGrow={1}>
-            <Autocomplete>
-              <Input
-                type="text"
-                placeholder="Current Location"
-                ref={originRef}
-              />
-            </Autocomplete>
-          </Box>
+      {isVisible && (
+        <HStack
+          marginTop={2}
+          p={2}
+          borderRadius="lg"
+          zIndex="1"
+          spacing={[
+            "150", // 0-30em
+            "300", // 30em-48em
+            "580", // 48em-62em
+            "800", // 62em+
+          ]}
+        >
+          <Text fontSize="2xl" as="b">
+            {" "}
+            Hi Kate!{" "}
+          </Text>
+          <Avatar name="Kate Simnac" alignItems />
         </HStack>
-      </Box>
-
+      )}
       {
-        // Destination Search Bar
+        // Return Button
       }
-      <Box
-        p={2}
-        m={1}
-        borderRadius="lg"
-        bgColor="white"
-        shadow="base"
-        zIndex="1"
-        width={[
-          "80%", // 0-30em
-          "50%", // 30em-48em
-          "25%", // 48em-62em
-          "15%", // 62em+
-        ]}
-      >
-        <HStack p={0.5} borderRadius="lg" zIndex="1">
-          <Icon as={FaMapMarkerAlt} boxSize={6} />
-          <Box flexGrow={1}>
-            <Autocomplete>
-              <Input type="text" placeholder="Where To?" ref={destinationRef} />
-            </Autocomplete>
-          </Box>
-        </HStack>
-      </Box>
-
+      {!isVisible && (
+        <Flex
+          p={2}
+          paddingTop={30}
+          paddingLeft={25}
+          position="absolute"
+          top={0}
+          left={0}
+          align="center"
+        >
+          <IconButton
+            aria-label="center back"
+            colorScheme="pink"
+            icon={<AiOutlineLeft />}
+            isRound
+            onClick={() => {
+              setIsVisible(true);
+            }}
+          />
+          <Text fontSize="2xl" as="b" paddingLeft={5}>
+            {" "}
+            Your Safest Directions!{" "}
+          </Text>
+        </Flex>
+      )}
+      {isVisible && (
+        <Box
+          p={2}
+          borderRadius="lg"
+          m={1}
+          bgColor="white"
+          shadow="base"
+          zIndex="1"
+          width={[
+            "80%", // 0-30em
+            "50%", // 30em-48em
+            "25%", // 48em-62em
+            "15%", // 62em+
+          ]}
+        >
+          <HStack p={0.5} borderRadius="lg" zIndex="1">
+            <Icon as={BiTargetLock} boxSize={6} />
+            <Box flexGrow={1}>
+              <Autocomplete>
+                <Input
+                  type="text"
+                  placeholder="Current Location"
+                  ref={originRef}
+                />
+              </Autocomplete>
+            </Box>
+          </HStack>
+        </Box>
+      )}
+      ;
+      {isVisible && (
+        <Box
+          p={2}
+          m={1}
+          borderRadius="lg"
+          bgColor="white"
+          shadow="base"
+          zIndex="1"
+          width={[
+            "80%", // 0-30em
+            "50%", // 30em-48em
+            "25%", // 48em-62em
+            "15%", // 62em+
+          ]}
+        >
+          <HStack p={0.5} borderRadius="lg" zIndex="1">
+            <Icon as={FaMapMarkerAlt} boxSize={6} />
+            <Box flexGrow={1}>
+              <Autocomplete>
+                <Input
+                  type="text"
+                  placeholder="Where To?"
+                  ref={destinationRef}
+                />
+              </Autocomplete>
+            </Box>
+          </HStack>
+        </Box>
+      )}
+      ;
       {
         // Functionality Button Group
       }
-      <ButtonGroup spacing={5} padding={1.5}>
-        <Button colorScheme="purple" type="submit" onClick={calculateRoute}>
-          Calculate Route
-        </Button>
-        <IconButton
-          aria-label="center back"
-          colorScheme="green"
-          icon={<FaLocationArrow />}
-          isRound
-          onClick={() => {
-            map.panTo(center);
-            map.setZoom(15);
-          }}
-        />
-        <IconButton
-          aria-label="center back"
-          colorScheme="red"
-          icon={<FaTimes />}
-          onClick={clearRoute}
-        />
-      </ButtonGroup>
+      {isVisible && (
+        <ButtonGroup spacing={5} padding={1.5}>
+          <Button
+            colorScheme="pink"
+            type="submit"
+            onClick={() => {
+              hideBoxes();
+              calculateRoute();
+              showReturnButton();
+            }}
+          >
+            Search!
+          </Button>
 
-      // For development purposes
+          <IconButton
+            aria-label="center back"
+            colorScheme="green"
+            icon={<FaLocationArrow />}
+            isRound
+            onClick={() => {
+              map.panTo(center);
+              map.setZoom(15);
+            }}
+          />
+
+          <IconButton
+            aria-label="center back"
+            colorScheme="red"
+            icon={<FaTimes />}
+            onClick={clearRoute}
+          />
+        </ButtonGroup>
+      )}
+      ; // For development purposes
       {/*<LoadingComponent/>*/}
-
       {
         // <HStack spacing={4} mt={4} justifyContent="space-between">
         //<Text>Distance: {distance} </Text>
         //<Text>Duration: {duration} </Text>
         //</HStack>
       }
-
       {
         // Cosmetic Button Group
       }
-      <ButtonGroup spacing={3} padding={1.5}>
-        <Button
-          leftIcon={<AiOutlineHome />}
-          colorScheme="purple"
-          variant="outline"
-          onClick={() => {
-            map.panTo(Home);
-            map.setZoom(15);
-          }}
-        >
-          Home
-        </Button>
+      {isVisible && (
+        <ButtonGroup spacing={3} padding={1.5}>
+          <Button
+            leftIcon={<AiOutlineHome />}
+            colorScheme="purple"
+            variant="outline"
+            onClick={() => {
+              map.panTo(Home);
+              map.setZoom(15);
+            }}
+          >
+            Home
+          </Button>
 
-        <Button
-          leftIcon={<RxCounterClockwiseClock />}
-          colorScheme="purple"
-          variant="outline"
-          onClick={() => {
-            map.panTo(Recent);
-            map.setZoom(15);
-          }}
-        >
-          Recent
-        </Button>
+          <Button
+            leftIcon={<RxCounterClockwiseClock />}
+            colorScheme="purple"
+            variant="outline"
+            onClick={() => {
+              map.panTo(Recent);
+              map.setZoom(15);
+            }}
+          >
+            Recent
+          </Button>
 
-        <Button
-          leftIcon={<MdFavoriteBorder />}
-          colorScheme="purple"
-          variant="outline"
-          onClick={() => {
-            map.panTo(Favorite);
-            map.setZoom(15);
-          }}
+          <Button
+            leftIcon={<MdFavoriteBorder />}
+            colorScheme="purple"
+            variant="outline"
+            onClick={() => {
+              map.panTo(Favorite);
+              map.setZoom(15);
+            }}
+          >
+            Favorite
+          </Button>
+        </ButtonGroup>
+      )}
+      ;
+      {
+        // Alert Buttons
+      }
+      {!isVisible && (
+        <ButtonGroup
+          spacing={1}
+          position="absolute"
+          bottom={0}
+          left={0}
+          right={0}
+          paddingBottom={65}
         >
-          Favorite
-        </Button>
-      </ButtonGroup>
+          <Button
+            leftIcon={<FaLocationArrow />}
+            colorScheme="purple"
+            variant="solid"
+            onClick={() => {
+              map.panTo(Home);
+              map.setZoom(15);
+            }}
+          >
+            Share Location
+          </Button>
 
-      {/* Available Routes */}
-      <VStack spacing={4} mt={4} justifyContent="space-between">
-        {directionsResponse && (
-          <>
-            <Text fontSize="lg" fontWeight="bold" mt={4}>
-              Available routes:
-            </Text>
-            {routes.map((route, index) => (
-              <Button
-                key={index}
-                width="auto"
-                height="auto"
-                justifyContent="flex-start"
-                textAlign="left"
-                bgColor="transparent"
-                _hover={{ bgColor: "gray.100" }}
-                onClick={() => handleButtonClick(index)}
-              >
-                <VStack>
-                  <Box pt={2}
-                       width='320px'
-                       height='130px'
-                       justifyContent='center'>
-                    <Flex>
-                      <div>{route.legs[0].duration.text}</div>
-                      <Spacer />
-                      <div>
-                        {now} - {calculateEnd(route.legs[0].duration.value)}
-                      </div>
-                    </Flex>
-                    <Text>
-                      Route {index + 1}: {route.summary}
-                    </Text>
-                    <Box pb={2}>
-                      <div>Distance: {route.legs[0].distance.text}</div>
+          <Button
+            leftIcon={<HiBellAlert />}
+            colorScheme="red"
+            variant="solid"
+            onClick={() => {
+              map.panTo(Recent);
+              map.setZoom(15);
+            }}
+          >
+            Alert
+          </Button>
+
+          <Button
+            leftIcon={<BiPhoneCall />}
+            colorScheme="pink"
+            variant="solid"
+            onClick={() => {
+              map.panTo(Favorite);
+              map.setZoom(15);
+            }}
+          >
+            Fake Call
+          </Button>
+        </ButtonGroup>
+      )}
+      ;{/* Available Routes */}
+      {!isVisible && (
+        <VStack spacing={4} mt={4} justifyContent="space-between">
+          {directionsResponse && (
+            <>
+              <Text fontSize="lg" fontWeight="bold" mt={4}>
+                Available routes:
+              </Text>
+              {routes.map((route, index) => (
+                <Button
+                  key={index}
+                  width="auto"
+                  height="auto"
+                  justifyContent="flex-start"
+                  textAlign="left"
+                  bgColor="white"
+                  _hover={{ bgColor: "gray.100" }}
+                  onClick={() => handleButtonClick(index)}
+                >
+                  <VStack>
+                    <Box
+                      pt={2}
+                      width="320px"
+                      height="130px"
+                      justifyContent="center"
+                    >
+                      <Flex>
+                        <div>{route.legs[0].duration.text}</div>
+                        <Spacer />
+                        <div>
+                          {now} - {calculateEnd(route.legs[0].duration.value)}
+                        </div>
+                      </Flex>
+                      <Text>
+                        Route {index + 1}: {route.summary}
+                      </Text>
+                      <Box pb={2}>
+                        <div>Distance: {route.legs[0].distance.text}</div>
+                      </Box>
+                      <Alert status="success" h="30px">
+                        <AlertIcon />
+                        <div>Danger Level: {getDangerLevel(route.summary)}</div>
+                      </Alert>
                     </Box>
-                    <Alert status='success' h='30px'>
-                      <AlertIcon />
-                      <div>Danger Level: {getDangerLevel(route.summary)}</div>
-                    </Alert>
-                  </Box>
-                  <div>
-                    Route {index + 1}: {route.summary}
-                  </div>
-                  {/* <div>Distance: {route.legs[0].distance.text}</div> */}
-                  {/* <div>Duration: {route.legs[0].duration.text}</div> */}
-                  
-                </VStack>
-              </Button>
-            ))}
+                    <div>
+                      Route {index + 1}: {route.summary}
+                    </div>
+                    {/* <div>Distance: {route.legs[0].distance.text}</div> */}
+                    {/* <div>Duration: {route.legs[0].duration.text}</div> */}
+                  </VStack>
+                </Button>
+              ))}
 
-            <DirectionsRenderer
-              directions={directionsResponse}
-              routeIndex={routeIndex}
-            />
-          </>
-        )}
-      </VStack>
+              <DirectionsRenderer
+                directions={directionsResponse}
+                routeIndex={routeIndex}
+              />
+            </>
+          )}
+        </VStack>
+      )}
+      ;
     </Flex>
   );
 }
